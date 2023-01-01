f0f2c04339ba947529377306a7c8338418ba4d76
mwybo <matt.wyborski@gmail.com>
2023-01-01 08:42:25 -0500

    Add commodities after regionwide AH merge

--- blizzardapi/wow/wow_game_data_api.py
+++ blizzardapi/wow/wow_game_data_api.py
@@ -72,6 +72,12 @@ class WowGameDataApi(Api):
         query_params = {"namespace": f"dynamic-{region}", "locale": locale}
         return super().get_resource(resource, region, query_params)
 
+    def get_commodities(self, region, locale):
+        """Return all active commodites auctions for a region."""
+        resource = "/data/wow/auctions/commodities"
+        query_params = {"namespace": f"dynamic-{region}", "locale": locale}
+        return super().get_resource(resource, region, query_params)
+
     # Azerite Essence API
 
     def get_azerite_essences_index(self, region, locale):
